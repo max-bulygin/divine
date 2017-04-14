adc55c6276cfcd619b210576000738ffd04b2e1b
Max Bulygin <maksym.bulygin@gmail.com>
2017-04-14 16:30:48 +0300

    basic markup and styling for 3Reasons section

--- assets/dev/js/scripts.js
+++ assets/dev/js/scripts.js
@@ -1,6 +1,3 @@
 //=require ../third-party/scrollreveal/dist/scrollreveal.min.js
 //=require ../third-party/vidbg/vidbg.min.js
-
-// YOUR CUSTOM JS GOES HERE
-
-alert("hello");
+//=require ../third-party/AniJS/dist/anijs-min.js
