9616c56d4ee8fa2d8a2e75752125ab8850f3e403
Max Bulygin <maksym.bulygin@gmail.com>
2017-04-17 22:51:11 +0300

    complete villas section all resolutions

--- assets/dev/js/scripts.js
+++ assets/dev/js/scripts.js
@@ -6,25 +6,25 @@
 
 var owl = $('.dest');
 owl.owlCarousel({
-    loop:true,
-    center:true,
+    loop: true,
+    center: true,
     stagePadding: 12,
-    autoplay:false,
-    autoplayTimeout:1000,
-    autoplayHoverPause:true,
+    autoplay: false,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true,
     dots: false,
-    responsive:{
-        0:{
-            items:1
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:2
+        600: {
+            items: 2
         },
-        980:{
-            items:3
+        980: {
+            items: 3
         },
-        1400:{
-            items:5
+        1400: {
+            items: 5
         }
     }
 });
@@ -33,7 +33,7 @@ var $right = $('.hover_control_right');
 var $left = $('.hover_control_left');
 var intID;
 
-$right.on('mouseover',function(){
+$right.on('mouseover', function () {
     intID = setInterval(function () {
         owl.trigger('next.owl.carousel', [300]);
     }, 1000);
@@ -47,40 +47,79 @@ $right.on('mouseout', function () {
     stop(intID);
 });
 
-$left.on('mouseover',function(){
+$left.on('mouseover', function () {
     intID = setInterval(function () {
         owl.trigger('prev.owl.carousel', [300]);
     }, 1000);
 });
-$left.on('mouseout',function(){
+$left.on('mouseout', function () {
     stop(intID);
 });
 
-$('.non_loop_rent').owlCarousel({
-    items:1,
-    loop:false,
-    margin:10,
+// RENT VILLAS
+
+var $rent = $('.non_loop_rent');
+
+$rent.owlCarousel({
+    items: 1,
+    loop: false,
+    margin: 10,
     responsive: {
-        640:{
-            items:2,
+        640: {
+            items: 2,
             autoWidth: true
+        },
+        1200: {
+            items: 1,
+            autoWidth: true,
+            margin: 40
         }
     }
 });
 
-$('.non_loop_buy').owlCarousel({
-    items:1,
-    loop:false,
-    margin:10,
+var $rentPrev = $('#rentPrev');
+var $rentNext = $('#rentNext');
+
+$rentPrev.on('click', function () {
+    $rent.trigger('prev.owl.carousel', [300]);
+});
+
+$rentNext.on('click', function () {
+    $rent.trigger('next.owl.carousel', [300]);
+});
+
+
+// BUY VILLAS
+
+var $buy = $('.non_loop_buy');
+
+$buy.owlCarousel({
+    items: 1,
+    loop: false,
+    margin: 10,
     rtl: true,
     responsive: {
-        640:{
-            items:2,
+        640: {
+            items: 2,
             autoWidth: true
         },
-        1200:{
-            items:1,
-            autoWidth: true
+        1200: {
+            items: 1,
+            autoWidth: true,
+            margin: 40
         }
     }
 });
+
+var $buyPrev = $('#buyPrev');
+var $buyNext = $('#buyNext');
+
+
+$buyPrev.on('click', function () {
+    $buy.trigger('prev.owl.carousel', [300]);
+});
+
+$buyNext.on('click', function () {
+    $buy.trigger('next.owl.carousel', [300]);
+});
+
